ed5de347b5e98e4a04d18db6c141fbe8c4c5add4
Alexey Khristolubov <alex-khrist98@mail.ru>
2023-09-27 15:21:58 +0500

    fix: убирает неиспользумые импорты

--- pages/base.py
+++ pages/base.py
@@ -1,6 +1,5 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 from selenium import webdriver 
-from selenium.webdriver.common.by import By
 
 class BasePage(ABC):
     """Этот абстрактный класс создаёт необходимые базовые методы для работы с тестируемой страницей. 
