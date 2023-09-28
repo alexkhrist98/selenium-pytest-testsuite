bc8675cdb9b82c659a476d84a57abbc322c16df4
Alexey Khristolubov <alex-khrist98@mail.ru>
2023-09-28 11:53:23 +0500

    fix: исправляет название теста, делает его более читаемым

--- tests/test_scenario1.py
+++ tests/test_scenario1.py
@@ -1,8 +1,9 @@
-from pages.sbismainpage import SbisMainPage, URL  
+from pages.sbismainpage import SbisMainPage
+from pages.sbiscontactspage import SbisContactsPage  
 
-def test_sbis_contacts(): 
+def test_sbis_go_to_contacts(): 
     """Этот тест проверяет переход на страницу контакты с главной страницы СБИС"""
     page = SbisMainPage() 
     page.navigate_to_url() 
     page.click_contacts_button() 
-    assert page.get_current_url() == URL + "contacts"
+    assert page.get_current_url() == page.url + "contacts"
