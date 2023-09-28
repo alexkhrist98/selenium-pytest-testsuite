e8f7dd6b0afe3a6e0cb9d71a212d2bc46515b19d
Alexey Khristolubov <alex-khrist98@mail.ru>
2023-09-28 21:18:25 +0500

    core: добавляет метод scroll_down_with_keyborad, make_screenshot  и make_screenshot_dir. Также добавляет папку для хранения скриншотов в обции веб драйвера

--- pages/base.py
+++ pages/base.py
@@ -1,5 +1,9 @@
 from abc import ABC
+from datetime import datetime 
+import os
 from selenium import webdriver 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 class BasePage(ABC):
     """Этот абстрактный класс создаёт необходимые базовые методы для работы с тестируемой страницей. 
@@ -8,10 +12,25 @@ class BasePage(ABC):
 
     def __init__(self, url: str):
         """Этот метод создаёт экземпляр класса веб-страница. В качесте аргументов он принимает адрес страницы. 
-        Он также инициализирует свойство self.driver, присваевая ему экземпляр класса webdriver.Edge"""
-        self.driver = webdriver.Edge()  
+        Он также инициализирует свойство self.driver, присваевая ему экземпляр класса webdriver.Edge
+        Также этот метод инициализирует опции веб-драйвера, в частности, местоположение скриншотов, которые
+        сохраняются при возникновнеии исключений."""
+        screenshot_dir = self.make_screenshot_dir()
+        driver_options = webdriver.EdgeOptions()
+        driver_options.add_argument(f"--screenshot-dir={screenshot_dir}")
+        self.driver = webdriver.Edge(options=driver_options)  
         self.url = url 
     
+    @staticmethod
+    def make_screenshot_dir():
+        """Этот статический метод создаёт папку, в которой будут храниться скриншоты, 
+        сохраняемые драйвером во врмея проведения тестов."""
+        screenshot_dir = os.path.join(os.getcwd(), "screenshots")
+        if not os.path.exists(screenshot_dir):
+            os.makedirs(screenshot_dir)
+        return screenshot_dir
+
+
     def navigate_to_url(self):
         """ЭЭтот метод вызывает метод webdriver.get и передаёт ему аргумент self.url"""
         return self.driver.get(self.url)
@@ -24,6 +43,16 @@ class BasePage(ABC):
             return self.driver.execute_script(f"window.scrollBy(0, {offset});")
         if not offset:
             return self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    
+    def scroll_down_with_keyboard(self, num_of_strokes: int = 1):
+        """Этот метод предлагает альтернативную имплементацию прокрутки страницы.
+        В качестве аргумента метод принимает количество нажатий на кнопку page downю 
+        Метод находит тег body и посылает ему клавишу page down, что позволяет прокручивать страницу. 
+        Метод может быть удалён после решения проблемы с выполнением JavaScript кода"""
+        body = self.driver.find_element(By.TAG_NAME, "body")
+        for i in range(0, num_of_strokes + 1):
+            body.send_keys(Keys.PAGE_DOWN)
+        
     
     def scroll_up(self, offset: int = 0): 
         """Этот метод позволяет прокрутить страницу вверх.
@@ -53,6 +82,11 @@ class BasePage(ABC):
     def close_browser(self): 
         """Этот метод позволяет закрыть браузер после завершения теста"""
         return self.driver.close() 
+    
+    def make_screenshot(self): 
+        """Этот метод созраняет скриншот страницы. 
+        Он будет использован при обработке исключений для сохранения информации о том, что произошло"""
+        return self.driver.save_screenshot(f"{__class__.__name__}: {datetime.now()}.png")
 
     def __str__(self):
         """Этот метод возвращает строку с используемым адресом страницы"""
