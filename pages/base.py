9d6d7e87533a11b4070357f921fd107d2065a735
Alexey Khristolubov <alex-khrist98@mail.ru>
2023-10-02 04:42:37 +0500

    core: добавляет метод get_title, добавляет документацию модуля

--- pages/base.py
+++ pages/base.py
@@ -1,10 +1,19 @@
+"""Этот модуль содержит в себе класс BasePage, который является базовым для всех страниц. 
+Для описания новой страницы, необходимо отнаследоваться от класса BasePage  и внутри 
+__init__ мутода новой страницы вызвать __init__ метод
+этого класса, передв в него URL желаемой страницы.
+Класс имплементирует ряд базовых методов - navigate_to_url, scroll_up, scroll_down, go_back, get_current_url и др.
+"""
+
 from abc import ABC
-from datetime import datetime 
+from datetime import datetime
 import os
-from selenium import webdriver 
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains 
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+
 
 class BasePage(ABC):
     """Этот абстрактный класс создаёт необходимые базовые методы для работы с тестируемой страницей. 
@@ -17,11 +26,11 @@ class BasePage(ABC):
         Также этот метод инициализирует опции веб-драйвера, в частности, местоположение скриншотов, которые
         сохраняются при возникновнеии исключений."""
         screenshot_dir = self._make_screenshot_dir()
-        driver_options = webdriver.ChromeOptions() 
+        driver_options = webdriver.ChromeOptions()
         driver_options.add_argument(f"--screenshot-dir={screenshot_dir}")
-        self.driver = webdriver.Chrome(options=driver_options) 
-        self.url = url 
-    
+        self.driver = webdriver.Chrome(options=driver_options)
+        self.url = url
+
     @staticmethod
     def _make_screenshot_dir():
         """Этот статический метод создаёт папку, в которой будут храниться скриншоты, 
@@ -31,19 +40,18 @@ class BasePage(ABC):
             os.makedirs(screenshot_dir)
         return screenshot_dir
 
-
     def navigate_to_url(self):
         """ЭЭтот метод вызывает метод webdriver.get и передаёт ему аргумент self.url"""
         return self.driver.get(self.url)
 
-    def scroll_down(self, offset: int): 
+    def scroll_down(self, offset: int):
         """Этот метод позволяет прокрутить страницу вниз на определённое количетсво пикселей. 
         В качестве аргумента он принимает offset - количество пикселей.
         ВНИМАНИЕ! Метод был переписан по причине невозможности выполнения JavaScript на сайте.
         Для прокрутки к конкретном элементу, используйте методы, определённыевнутри подклассов страниц."""
         return ActionChains(self.driver).scroll_by_amount(0, offset).perform()
 
-    def scroll_up(self, offset: int = 0): 
+    def scroll_up(self, offset: int = 0):
         """Этот метод позволяет прокрутить страницу вверх на заданное количетсво пикселей.
         Он принимает аргумент offset - количество пикселей. 
         Для прокрутки к конкретному элементу, используйте методы, определённые в подклассах."""
@@ -51,12 +59,13 @@ class BasePage(ABC):
 
     def go_back(self):
         """Этот метод позволяет вернуться на одну страницу назад."""
-        return self.driver.back() 
-    def get_current_url(self): 
+        return self.driver.back()
+
+    def get_current_url(self):
         """Этот метод возвращается адрес страницы, на которой находится драйвер в момент вызова этого метода"""
         return self.driver.current_url
 
-    def switch_tab(self, tab_number: int = 1): 
+    def switch_tab(self, tab_number: int = 1):
         """Этот метод позволяет переключаться между вкладками окна драйвера. 
         В качестве аргумента он принимает номер вкладки. По умолчанию метод переключает на следующую вкладку.
         Индексация вкладок выглдяти так:
@@ -64,19 +73,26 @@ class BasePage(ABC):
         -Под индексом 1 - следующаяю 
         -Под индексом -1 - предыдущая."""
         return self.driver.switch_to.window(self.driver.window_handles[tab_number])
-    
-    def close_browser(self): 
+
+    def close_browser(self):
         """Этот метод позволяет закрыть браузер после завершения теста"""
-        return self.driver.close() 
-    
-    def make_screenshot(self): 
+        return self.driver.close()
+
+    def make_screenshot(self):
         """Этот метод созраняет скриншот страницы. 
         Он будет использован при обработке исключений для сохранения информации о том, что произошло"""
         return self.driver.save_screenshot(self._make_screenshot_dir() + f"\\{self.__class__.__name__}: {datetime.now()}.png")
 
+    def wait_for_page_reload(self):
+        """Этот метод позволяет дождаться перезагрузки страницы. 
+        Он определяет перезагрузку по появлению элемента body."""
+        delay = WebDriverWait(self.driver, 10)
+        return delay.until(expected_conditions.presence_of_element_located((By.TAG_NAME, "body")))
+
+    def get_title(self):
+        """Этот метод возвращает содержимое элемента title в блоке head страницы."""
+        return self.driver.title
+
     def __str__(self):
         """Этот метод возвращает строку с используемым адресом страницы"""
         return f"{self.__class__.__name__}({self.url})"
-        
-
-        
